82b01629a629b1eea881323cb009fe0c2fb0b95f
Outboet <99953858+Ouboet360@users.noreply.github.com>
2025-08-10 20:08:04 +0200

    Update scritp.js

--- Jquery demo/scritp.js
+++ Jquery demo/scritp.js
@@ -3,10 +3,19 @@ let excelData = [];
 $('#excelFile').on('change', function(e) {
   const file = e.target.files[0];
   const reader = new FileReader();
+  const fileName = file.name.toLowerCase();
 
   reader.onload = function(e) {
-    const data = new Uint8Array(e.target.result);
-    const workbook = XLSX.read(data, { type: 'array' });
+    let workbook;
+
+    if (fileName.endsWith('.csv')) {
+      const csvText = e.target.result;
+      workbook = XLSX.read(csvText, { type: 'string' });
+    } else {
+      const data = new Uint8Array(e.target.result);
+      workbook = XLSX.read(data, { type: 'array' });
+    }
+
     const sheetName = workbook.SheetNames[0];
     const sheet = workbook.Sheets[sheetName];
     excelData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
@@ -19,7 +28,11 @@ $('#excelFile').on('change', function(e) {
     });
   };
 
-  reader.readAsArrayBuffer(file);
+  if (fileName.endsWith('.csv')) {
+    reader.readAsText(file);
+  } else {
+    reader.readAsArrayBuffer(file);
+  }
 });
 
 $('#searchInput').on('keyup', function() {
